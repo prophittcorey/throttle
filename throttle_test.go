a452715a66a995a077bb18e2ef5964ba9f11419b
Corey Prophitt <git@prophitt.me>
2023-01-04 08:48:48 -0800

    Added a basic test

--- throttle_test.go
+++ throttle_test.go
@@ -1,6 +1,26 @@
 package throttle
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestThrottle(t *testing.T) {
+	executor := New(100)
+
+	start := time.Now()
+
+	for i := 1; i < 1000; i++ {
+		executor.Run(func() {
+			time.Sleep(1 * time.Second)
+		})
+	}
+
+	executor.Wait()
+
+	took := time.Since(start)
+
+	if took < (10*time.Second) || took > (11*time.Second) {
+		t.Fatalf("failed to execute in ~10 seconds; took %v", took)
+	}
 }
